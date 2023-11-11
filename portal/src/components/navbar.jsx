389d26652c9ef8d98823a9f4f1a79a6dcf0602f2
shashidhar <shashidharreddy.p@pangeanglobal.com>
2023-11-11 13:22:38 +0530

    --home page setup

---
+++ portal/src/components/navbar.jsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <a href="#work">Work</a>
+      <a href="#about">About</a>
+      <a href="#team">Team</a>
+      <a href="ideas">Ideas</a>
+      <a href="Carrers">Carrers</a>
+    </div>
+  );
+};
+export default Navbar;
